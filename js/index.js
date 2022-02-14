7bce6db51ff89f5b30e12a29a6002b20393ee47a
andycrafts95 <88752407+andycrafts95@users.noreply.github.com>
2022-02-14 14:55:11 -0500

    trying to get github to get page right

--- js/index.js
+++ js/index.js
@@ -1,2 +1,28 @@
-//index.js
-console.log ("your index.js file is loaded correctly");
+// index.js
+console.log ("Your index.js file is loaded correctly");
+//HW 19 JQuery Listeners
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("downloadResume");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
